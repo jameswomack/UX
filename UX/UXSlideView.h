d71f690ee157a50fd4f5b2bec9a11c9cc0b28f88
James Womack <jamesw@amcoonline.net>
2013-02-22 22:05:49 -0800

    Ignorance

---
+++ UX/UXSlideView.h
@@ -0,0 +1,13 @@
+//
+//  UXSlideView.h
+//  UX
+//
+//  Created by James Womack on 2/22/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UXSlideView : UIView
+
+@end
---
+++ UX/UXSlideView.m
@@ -0,0 +1,31 @@
+//
+//  UXSlideView.m
+//  UX
+//
+//  Created by James Womack on 2/22/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import "UXSlideView.h"
+
+@implementation UXSlideView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
